d87dfbc7cb8c79c65d19d2213d88af3eec770ec2
Youma <youma@core>
2025-10-28 15:13:10 -0300

    cuarto commit

--- Miapp/admin.py
+++ Miapp/admin.py
@@ -2,50 +2,37 @@ from django.contrib import admin
 from .models import Calificacion, CargaMasiva, Usuario, Rol, Busqueda, Factor_Val, Permiso, InstrumentoNI, Auditoria, RolPermiso
 
 class CalificacionAdmin(admin.ModelAdmin):
-
-    list_display = ['calid','monto', 'factor', 'periodo', 'instrumento', 'estado', 'fecha_creacion', 'fecha_modificacion', 'usuario_id_usuario', 'factor_val_id_factor' ]
+    list_display = ['calid', 'monto', 'factor', 'periodo', 'instrumento', 'estado', 
+                    'fecha_creacion', 'fecha_modificacion', 'usuario_id_usuario__nombre', 
+                    'factor_val_id_factor__descripcion']
 
 class CargaMasivaAdmin(admin.ModelAdmin):
-
     list_display = ['id_cm', 'archivo', 'errores', 'calificacion_calid']
-    
-class UsuarioAdmin(admin.ModelAdmin):
 
-    list_display = ['nombre', 'email', 'rol_id', 'activo']
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'email', 'rol_id__nombre_rol', 'activo']
 
 class RolAdmin(admin.ModelAdmin):
-
-    list_display = ['rol', 'permiso', 'descripcion_rol' ]
+    list_display = ['nombre_rol', 'descripcion_rol']
 
 class BusquedaAdmin(admin.ModelAdmin):
-
-    list_display = ['id_busqueda', 'criterios_busqueda', 'usuario_id_usuario']
+    list_display = ['id_busqueda', 'criterios_busqueda', 'usuario_id_usuario__nombre']
 
 class Factor_valAdmin(admin.ModelAdmin):
-
     list_display = ['id_factor', 'rango_minimo', 'rango_maximo', 'descripcion']
-class CalificacionAdmin(admin.ModelAdmin):
-
-    list_display = []
 
 class PermisoAdmin(admin.ModelAdmin):
-
     list_display = ['id_permiso', 'nombre', 'descripcion_permiso']
 
 class InstrumentoNIAdmin(admin.ModelAdmin):
-
     list_display = ['id_instru', 'nombre', 'regla_es', 'estado']
 
 class AuditoriaAdmin(admin.ModelAdmin):
-    
-    list_display = ['id_Auditoria', 'accion', 'Tabla', 'Cambios', 'Fecha', 'ip', 'Firma_Digital', 'usuario_id_usuario']
-    
-    
-class Rol_permisoAdmin(admin.ModelAdmin):
-    
+    list_display = ['id_Auditoria', 'accion', 'Tabla', 'Cambios', 'Fecha', 'ip', 
+                    'Firma_Digital', 'usuario_id_usuario__nombre']
+
+class RolPermisoAdmin(admin.ModelAdmin):
     list_display = ['rol', 'permiso']
-
-
 
 
 admin.site.register(Calificacion, CalificacionAdmin)
@@ -57,4 +44,4 @@ admin.site.register(Factor_Val, Factor_valAdmin)
 admin.site.register(Permiso, PermisoAdmin)
 admin.site.register(InstrumentoNI, InstrumentoNIAdmin)
 admin.site.register(Auditoria, AuditoriaAdmin)
-admin.site.register(RolPermiso, Rol_permisoAdmin)
+admin.site.register(RolPermiso, RolPermisoAdmin)
